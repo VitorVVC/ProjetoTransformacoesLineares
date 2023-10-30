72958d65eb6b593815526dc68e21d1a097cce2c3
Vitor <vitorvvc2005@gmail.com>
2023-10-29 22:58:45 -0300

    Adicionando Comentários a main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,13 +1,20 @@
 package application;
 
 
+import java.util.Locale;
+
 import static application.Util.*;
 
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
 
-
+        // Puxa o método 'printSetado();' que nada mais é que o trabalho em um print.
         printSetado();
+
+        // Puxa o método 'interacao();' que nada mais é que ter um contato o usuário que está testando o progama.
+        //interacao();
+
 
     }
 }
